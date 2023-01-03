8fd543b34ce897fccab68747a500d1af16ca1942
Scorpioffi <leovejux95@gmail.com>
2023-01-03 13:51:05 +0100

    push grille

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ for(let i = 0; i < 240; i++) {
     let quadrado = document.createElement("div");
     grade.appendChild(quadrado);
 }
+
+const quadrados = document.querySelectorAll("#grade div");
+const invasores = [
+    0,1,2,3,4,5,6,7,8,9,10,11,
+    15,16,17,18,19,20,21,22,23,24,25,26,
+    30,31,32,33,34,35,36,37,38,39,40,41
+];
+
+let posicaoJogador = 202;
+let invasoresId;
+
+quadrados[posicaoJogador].classList.add("jogador")
+
+invasores.forEach(invasor => {
+    quadrados[invasor].classList.add("invasor");
+});
+
+document.addEventListener("keydown", moverJogador);
+invasoresId = setInterval(moverInvasores, 300);
+document.addEventListener("keyup", atirar);
