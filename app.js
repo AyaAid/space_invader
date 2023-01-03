435f45888bf6fae2f3653c4a47368fcfe7271682
AyaAid <aya.lakhel11@gmail.com>
2023-01-03 16:09:58 +0100

    happy dance

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const grade = document.getElementById("grade");
 const game = document.getElementById("game");
+let getDown = false;
+let direction = 1;
+let touch = [];
 
 for(let i = 0; i < 320; i++) {
     let board = document.createElement("div");
@@ -14,7 +17,7 @@ const ennemy = [
 ];
 
 let pos_ship = 280;
-let invasoresId;
+let ennemyId;
 
 board[pos_ship].classList.add("ship");
 
@@ -22,7 +25,7 @@ ennemy.forEach(ennemy => {
     board[ennemy].classList.add("ennemy");
 });
 
-
+ennemyId = setInterval(happyDance, 300);
 
 function tir(){
     let pos = pos_ship;
@@ -36,12 +39,60 @@ function tir(){
             board[pos].classList.add("boom");
             clearInterval(tir);
             setTimeout(() => board[pos].classList.remove("boom"), 400);
+            touch.push(ennemy.indexOf(pos));
         }
     }, 100);
-}
+};
 
 document.addEventListener("keydown", (e) => {
     if(e.keyCode === 32){
         tir();
     }
 });
+
+function happyDance() {
+    const debut_ligne = ennemy[0] % 16 == 0;
+    const fin_ligne = ennemy[ennemy.length - 1] % 16 == 15;
+
+    ennemy.forEach(ennemy => {
+        board[ennemy].classList.remove("ennemy");
+    });
+
+    if(debut_ligne && direction == -1) {
+        direction = 1;
+        getDown = true;
+    } else if (fin_ligne && direction == 1) {
+        direction = -1;
+        getDown = true;
+    }
+
+    for(let i = 0; i < ennemy.length; i++) {
+        ennemy[i] += getDown ? 16 : direction;
+
+    }
+
+    getDown = false;
+
+    ennemy.forEach((ennemy, indice) => {
+        if (!touch.includes(indice)) {
+            board[ennemy].classList.add("ennemy");
+        }
+    });
+
+    if (ennemy[ennemy.length -1] > board.length - 16) {
+        alert("Perdu ! test");
+        clearInterval(ennemyId);
+    }
+
+    if (board[pos_ship].classList.contains("ennemy")) {
+        alert("Perdu ! test");
+        board[pos_ship].classList.add("boum");
+        clearInterval(ennemyId);
+    }
+
+    if (touch.length == ennemy.length) {
+        alert("Gagné ! test");
+        clearInterval(ennemyId);
+    }
+
+}
