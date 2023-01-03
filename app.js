1f4a6f28d6a06ed8582dc20860da50d6dd7ca5ec
Scorpioffi <leovejux95@gmail.com>
2023-01-03 14:07:22 +0100

    pushpushpush

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const grade = document.getElementById("grade");
 const pontuacao = document.getElementById("pontuacao");
 
-for(let i = 0; i < 240; i++) {
+for(let i = 0; i < 320; i++) {
     let quadrado = document.createElement("div");
     grade.appendChild(quadrado);
 }
 
 const quadrados = document.querySelectorAll("#grade div");
 const invasores = [
-    0,1,2,3,4,5,6,7,8,9,10,11,
-    15,16,17,18,19,20,21,22,23,24,25,26,
-    30,31,32,33,34,35,36,37,38,39,40,41
+    2,3,4,5,6,7,8,9,10,11,12,13,
+    18,19,20,21,22,23,24,25,26,27,28,29,
+    34,35,36,37,38,39,40,41,42,43,44,45
 ];
 
-let posicaoJogador = 202;
+let posicaoJogador = 280;
 let invasoresId;
 
 quadrados[posicaoJogador].classList.add("jogador")
