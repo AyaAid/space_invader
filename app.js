83f0d3e27ca3c5fd948f257931973f201f5a4f59
Scorpioffi <leovejux95@gmail.com>
2023-01-03 12:14:08 +0100

    grille

---
+++ app.js
@@ -0,0 +1,7 @@
+const grade = document.getElementById("grade");
+const pontuacao = document.getElementById("pontuacao");
+
+for(let i = 0; i < 240; i++) {
+    let quadrado = document.createElement("div");
+    grade.appendChild(quadrado);
+}
